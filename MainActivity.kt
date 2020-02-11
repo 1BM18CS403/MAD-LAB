95118a8a3d73f40108be7f55a221cc1c1d88012b
1BM18CS403 <54163340+1BM18CS403@users.noreply.github.com>
2020-02-11 16:01:10 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -8,23 +8,32 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    //var ch = 1
-    //var font = 30f
+    var ch = 1
+    var font = 30f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val t = findViewById<View>(R.id.textView) as TextView
+        val t = findViewById<View>(R.id.text1) as TextView
         val b1 =
-            findViewById<View>(R.id.button) as Button
-
+            findViewById<View>(R.id.button1) as Button
+        b1.setOnClickListener {
+            t.textSize = font
+            font = font + 5
+            if (font == 50f) font = 30f
+        }
         val b2 =
-            findViewById<View>(R.id.button) as Button
-        b2.setOnClickListener { t.setTextColor(Color.GREEN)
-            val b1 =
-                findViewById<View>(R.id.button2) as Button
-
-            val b2 =
-                findViewById<View>(R.id.button2) as Button
-            b2.setOnClickListener { t.setTextColor(Color.BLUE)}
+            findViewById<View>(R.id.button2) as Button
+        b2.setOnClickListener {
+            when (ch) {
+                1 -> t.setTextColor(Color.RED)
+                2 -> t.setTextColor(Color.GREEN)
+                3 -> t.setTextColor(Color.BLUE)
+                4 -> t.setTextColor(Color.CYAN)
+                5 -> t.setTextColor(Color.YELLOW)
+                6 -> t.setTextColor(Color.MAGENTA)
+            }
+            ch++
+            if (ch == 7) ch = 1
+        }
     }
-}}
+}
